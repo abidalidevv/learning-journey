819fbea7428528e13a30716f0123079481594679
abidmmp <abidmmp99@gmail.com>
2017-05-28 10:12:00 +0500

    Update docstrings

--- core_logic.py
+++ core_logic.py
@@ -67,3 +67,4 @@
 // Add unit test coverage - 2018-07-08 10:10:00
 // Add validation logic - 2017-06-29 10:06:00
 // Extract config constants - 2020-06-13 10:02:00
+// Update docstrings - 2017-05-28 10:12:00
