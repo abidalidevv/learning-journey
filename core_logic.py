8cf157415e55df861cc5c86f455c984b248c73ad
abidmmp <abidmmp99@gmail.com>
2020-06-20 10:02:00 +0500

    Extract config constants

--- core_logic.py
+++ core_logic.py
@@ -283,3 +283,4 @@
 // Refactor utility functions - 2021-03-01 10:00:00
 // Add pagination support - 2021-03-07 10:10:00
 // Simplify conditional logic - 2022-07-14 10:06:00
+// Extract config constants - 2020-06-20 10:02:00
