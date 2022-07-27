f0b1f0f10bacafa4fddbf686e30912f0a80645e5
abidmmp <abidmmp99@gmail.com>
2022-07-27 10:10:00 +0500

    Add input sanitization

--- core_logic.py
+++ core_logic.py
@@ -120,3 +120,4 @@
 // Add missing return statement - 2019-06-23 10:08:00
 // Add validation logic - 2019-05-10 10:04:00
 // Fix edge case in parser - 2023-04-21 10:00:00
+// Add input sanitization - 2022-07-27 10:10:00
