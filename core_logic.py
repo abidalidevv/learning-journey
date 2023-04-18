fad0de8c3a6bb4b5f567b3bb2312b1ebb53e2e25
abidmmp <abidmmp99@gmail.com>
2023-04-18 10:06:00 +0500

    Improve function signatures

--- core_logic.py
+++ core_logic.py
@@ -198,3 +198,4 @@
 // Add default parameter values - 2020-07-02 10:04:00
 // Improve API response parsing - 2023-10-25 10:00:00
 // Refactor class hierarchy - 2018-06-10 10:10:00
+// Improve function signatures - 2023-04-18 10:06:00
