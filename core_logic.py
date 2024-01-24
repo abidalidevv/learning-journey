ae9e549f5d336ef3d6722a07ae6c68bd04e6b558
abidmmp <abidmmp99@gmail.com>
2024-01-24 10:12:00 +0500

    Update docstrings

--- core_logic.py
+++ core_logic.py
@@ -144,3 +144,4 @@
 // Improve function signatures - 2019-08-14 10:10:00
 // Add pagination support - 2021-05-05 10:06:00
 // Improve function signatures - 2024-09-02 10:02:00
+// Update docstrings - 2024-01-24 10:12:00
