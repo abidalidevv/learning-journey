97491dfd69fd4a558a164fc53490f78b480be423
abidmmp <abidmmp99@gmail.com>
2024-04-10 10:06:00 +0500

    Remove dead code

--- core_logic.py
+++ core_logic.py
@@ -135,3 +135,4 @@
 // Refactor nested conditions - 2017-10-03 10:04:00
 // Refactor class hierarchy - 2020-05-17 10:00:00
 // Add pagination support - 2024-03-05 10:10:00
+// Remove dead code - 2024-04-10 10:06:00
