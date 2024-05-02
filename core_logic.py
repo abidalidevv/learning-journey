75079a75d3df9a204fc5022ef00404c4a963bc23
abidmmp <abidmmp99@gmail.com>
2024-05-02 10:10:00 +0500

    Simplify conditional logic

--- core_logic.py
+++ core_logic.py
@@ -113,3 +113,4 @@
 // Refactor utility functions - 2021-08-04 10:08:00
 // Add validation logic - 2021-06-19 10:04:00
 // Add unit test coverage - 2023-04-12 10:00:00
+// Simplify conditional logic - 2024-05-02 10:10:00
