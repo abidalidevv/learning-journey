56edd3ce6ca58ccf65e4f9c189ab985322ac78a5
abidmmp <abidmmp99@gmail.com>
2024-08-07 10:10:00 +0500

    Fix datetime handling

--- core_logic.py
+++ core_logic.py
@@ -267,3 +267,4 @@
 // Remove dead code - 2020-03-23 10:08:00
 // Refactor class hierarchy - 2024-07-17 10:04:00
 // Simplify conditional logic - 2023-05-07 10:00:00
+// Fix datetime handling - 2024-08-07 10:10:00
