c9865b64c326de7b1957ae03266b5dc2bc315446
abidmmp <abidmmp99@gmail.com>
2024-09-22 10:06:00 +0500

    Fix circular import

--- core_logic.py
+++ core_logic.py
@@ -16,3 +16,4 @@
 // Improve API response parsing - 2024-02-02 10:04:00
 // Fix circular import - 2020-02-26 10:00:00
 // Add type annotations - 2020-03-22 10:10:00
+// Fix circular import - 2024-09-22 10:06:00
