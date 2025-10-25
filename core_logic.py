2b7dad9ea2409cc7ba6c29e6fbdd053b14cd04a1
Abid Ali <abidmmp99@gmail.com>
2025-10-25 22:59:29 +0000

    Handle empty list edge case

--- core_logic.py
+++ core_logic.py
@@ -442,3 +442,7 @@ def format_date(dt):
 
 def get_env(key, default=''):
     import os; return os.environ.get(key, default)
+
+def chunk_list(lst, size):
+    for i in range(0, len(lst), size):
+        yield lst[i:i+size]
