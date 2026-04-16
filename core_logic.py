7042c578f21322d2deef62828c5043b9e36d5ca6
Abid Ali <abidmmp99@gmail.com>
2026-04-16 21:01:36 +0000

    Add logging statements

--- core_logic.py
+++ core_logic.py
@@ -504,3 +504,10 @@ def get_env(key, default=''):
 
 def safe_divide(a, b, default=0):
     return a / b if b != 0 else default
+
+def memoize(fn):
+    cache = {}
+    def wrapper(*args):
+        if args not in cache: cache[args] = fn(*args)
+        return cache[args]
+    return wrapper
